b27939c5361c5a5cea012e4e30ae4af119f707c0
Daniel <danideveloperweb@gmail.com>
2025-03-02 12:34:02 +0100

    loading

--- src/app/core/services/loading.service.spec.ts
+++ src/app/core/services/loading.service.spec.ts
@@ -1,16 +1,60 @@
 import { TestBed } from '@angular/core/testing';
-
 import { LoadingService } from './loading.service';
 
 describe('LoadingService', () => {
   let service: LoadingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [LoadingService]
+    });
+
     service = TestBed.inject(LoadingService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should initialize with "false" state', (done) => {
+    service.loading$.subscribe((isLoading) => {
+      expect(isLoading).toBeFalse();
+      done();
+    });
+  });
+
+  it('should set state to "true" when show() is called', (done) => {
+    service.show();
+
+    service.loading$.subscribe((isLoading) => {
+      expect(isLoading).toBeTrue();
+      done();
+    });
+  });
+
+  it('should set state to "false" when hide() is called', (done) => {
+    service.show();
+    service.hide();
+
+    service.loading$.subscribe((isLoading) => {
+      expect(isLoading).toBeFalse();
+      done();
+    });
+  });
+
+  it('should emit values in the correct sequence: "false" → "true" → "false"', (done) => {
+    const expectedSequence = [false, true, false];
+    let index = 0;
+
+    service.loading$.subscribe((isLoading) => {
+      expect(isLoading).toBe(expectedSequence[index]);
+      index++;
+      if (index === expectedSequence.length) {
+        done();
+      }
+    });
+
+    service.show();
+    service.hide();
   });
 });
--- src/app/core/services/loading.service.ts
+++ src/app/core/services/loading.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class LoadingService {
+  private readonly _loading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public readonly loading$: Observable<boolean> = this._loading.asObservable();
 
-  constructor() { }
+  public show(): void {
+    this._loading.next(true);
+  }
+
+  public hide(): void {
+    this._loading.next(false);
+  }
 }
